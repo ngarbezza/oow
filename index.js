dfec821cd6273e4a31dad400c5f9572010d98d94
Nahuel Garbezza <n.garbezza@gmail.com>
2018-09-14 10:44:25 -0300

    Add first and last function; Avoid changing prototypes hierarchy

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 Collection = {
   isEmpty() { return this.length === 0; },
   notEmpty() { return !this.isEmpty(); },
+  first() { return this[0]; },
+  last() { return this[this.length-1]; },
 };
 
-String.prototype.__proto__ = Collection;
-Array.prototype.__proto__ = Collection;
+// ideal implementation, not recommended
+//Object.setPrototypeOf(Array.prototype, Collection);
+//Object.setPrototypeOf(String.prototype, Collection);
+
+// implementation w/o changing prototypes hierarchy
+Object.keys(Collection).forEach(function (methodName) {
+  Array.prototype[methodName] = Collection[methodName];
+  String.prototype[methodName] = Collection[methodName];
+});
