a4cf5ec2d067b3652f219d8de6537d27200edd1d
Nahuel Garbezza <n.garbezza@gmail.com>
2018-09-14 16:26:06 -0300

    Add any() and all() messages; add very basic test script

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-Collection = {
+const Collection = {
   isEmpty() { return this.length === 0; },
   notEmpty() { return !this.isEmpty(); },
   first() { return this[0]; },
   last() { return this[this.length-1]; },
+  any(predicate) {
+    for (let elem in this)
+      if (predicate(this[elem])) return true;
+    return false;
+  },
+  all(predicate) {
+    return !this.any(elem => !predicate(elem));
+  }
 };
 
 // ideal implementation, not recommended
@@ -11,6 +19,7 @@ Collection = {
 
 // implementation w/o changing prototypes hierarchy
 Object.keys(Collection).forEach(function (methodName) {
-  Array.prototype[methodName] = Collection[methodName];
-  String.prototype[methodName] = Collection[methodName];
+  [Array.prototype, String.prototype].forEach(function (proto) {
+    Object.defineProperty(proto, methodName, { value: Collection[methodName] });
+  });
 });
---
+++ tests.js
@@ -0,0 +1,47 @@
+require('./index');
+
+function suite() {
+  let total = arguments.length;
+  let passed = 0;
+  for (let result in arguments)
+    if (arguments[result]) passed++;
+  let failed = total - passed;
+  console.log(`${total} tests, ${passed} passed, ${failed} failed`);
+}
+
+function assert(message, actual, expected) {
+  let success = actual === expected;
+  if (success) {
+    console.log(`[OK] ${message}`);
+  } else {
+    console.log(`[FAIL] ${message}. Expected ${expected} got ${actual}`);
+  }
+  return success;
+}
+
+suite(
+  // isEmpty
+  assert('list is empty', [].isEmpty(), true),
+  assert('string is empty', "".isEmpty(), true),
+  // notEmpty
+  assert('list is not empty', [1].notEmpty(), true),
+  assert('string is not empty', "a".notEmpty(), true),
+  // first
+  assert('first of list', [1, 2, 3].first(), 1),
+  assert('first of string', "hola".first(), 'h'),
+  // last
+  assert('last of list', [1, 2, 3].last(), 3),
+  assert('last of string', "hola".last(), 'a'),
+  // any
+  assert('any in case true - lists', [1,2,3].any(num => num > 2), true),
+  assert('any in case true - string', "hola".any(letter => letter === "o"), true),
+  assert('any in case false - lists', [1,2,3].any(num => num > 5), false),
+  assert('any in case false - lists', "hola".any(letter => letter === 'w'), false),
+  assert('any in case empty', [].any(_ => false), false),
+  // all
+  assert('all in case true - lists', [1,2,3].all(num => num < 4), true),
+  assert('all in case true - string', "hola".all(letter => letter.match(/[a-z]/i)), true),
+  assert('all in case false - lists', [1,2,3].all(num => num > 2), false),
+  assert('all in case false - lists', "aaah".all(letter => letter === "a"), false),
+  assert('all in case empty', [].all(_ => false), true),
+);
