08f21a14a0eea5d0f66bf23d62df5db4ed8d9aef
Nahuel Garbezza <n.garbezza@gmail.com>
2018-09-17 00:48:10 -0300

    Add take(), drop() (Array, String) and equals() (Array)

--- oow.js
+++ oow.js
@@ -1,25 +1,53 @@
-const Collection = {
-  isEmpty() { return this.length === 0; },
-  notEmpty() { return !this.isEmpty(); },
-  first() { return this[0]; },
-  last() { return this[this.length-1]; },
-  any(predicate) {
-    for (let elem in this)
-      if (predicate(this[elem])) return true;
-    return false;
-  },
-  all(predicate) {
-    return !this.any(elem => !predicate(elem));
-  }
-};
-
-// ideal implementation, not recommended
-//Object.setPrototypeOf(Array.prototype, Collection);
-//Object.setPrototypeOf(String.prototype, Collection);
-
-// implementation w/o changing prototypes hierarchy
-Object.keys(Collection).forEach(function (methodName) {
-  [Array.prototype, String.prototype].forEach(function (proto) {
-    Object.defineProperty(proto, methodName, { value: Collection[methodName] });
+(function () {
+  const Collection = {
+    isEmpty() { return this.length === 0; },
+    notEmpty() { return !this.isEmpty(); },
+    first() { return this[0]; },
+    last() { return this[this.length-1]; },
+    any(predicate) {
+      for (let elem in this)
+        if (predicate(this[elem])) return true;
+      return false;
+    },
+    all(predicate) {
+      return !this.any(elem => !predicate(elem));
+    },
+    take(n) { return this.slice(0, n); },
+    drop(n) { return this.slice(n, this.length); }
+  };
+  
+  const ArrayExtensions = {
+    equals(array) {
+      if (!array) return false;
+      if (this.length !== array.length) return false;
+      
+      for (let i = 0, l=this.length; i < l; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+          if (!this[i].equals(array[i])) return false;
+        }
+        else if (this[i] !== array[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+  
+  let eachExtensionOf = function(extension, block) {
+    Object.keys(extension).forEach(block)
+  };
+  
+  let extend = function(proto, extension, methodName) {
+    Object.defineProperty(proto, methodName, { value: extension[methodName] });
+  };
+  
+  eachExtensionOf(Collection, function (methodName) {
+    [Array.prototype, String.prototype].forEach(function (proto) {
+      extend(proto, Collection, methodName);
+    });
   });
-});
+  
+  eachExtensionOf(ArrayExtensions, function (methodName) {
+    extend(Array.prototype, ArrayExtensions, methodName);
+  });
+})();
--- tests.js
+++ tests.js
@@ -9,39 +9,62 @@ function suite() {
   console.log(`${total} tests, ${passed} passed, ${failed} failed`);
 }
 
-function assert(message, actual, expected) {
-  let success = actual === expected;
-  if (success) {
+function assert(message, matcher) {
+  if (matcher.success) {
     console.log(`[OK] ${message}`);
   } else {
-    console.log(`[FAIL] ${message}. Expected ${expected} got ${actual}`);
+    console.log(`[FAIL] ${message}. ${matcher.errorMessage}`);
   }
-  return success;
+  return matcher.success;
+}
+
+function equal(actual, expected) {
+  let result;
+  if (expected instanceof Array) {
+    result = actual.equals(expected);
+  } else {
+    result = actual === expected;
+  }
+  return { success: result, errorMessage: `Expected ${expected} got ${actual}` };
+}
+
+function isTrue(value) {
+  return { success: value, errorMessage: `Expected ${value} to be true` };
+}
+
+function isFalse(value) {
+  return { success: !value, errorMessage: `Expected ${value} to be false` };
 }
 
 suite(
   // isEmpty
-  assert('list is empty', [].isEmpty(), true),
-  assert('string is empty', "".isEmpty(), true),
+  assert('list is empty',   isTrue([].isEmpty())),
+  assert('string is empty', isTrue("".isEmpty())),
   // notEmpty
-  assert('list is not empty', [1].notEmpty(), true),
-  assert('string is not empty', "a".notEmpty(), true),
+  assert('list is not empty',   isTrue([1].notEmpty())),
+  assert('string is not empty', isTrue("a".notEmpty())),
   // first
-  assert('first of list', [1, 2, 3].first(), 1),
-  assert('first of string', "hola".first(), 'h'),
+  assert('first of list',   equal([1, 2, 3].first(), 1)),
+  assert('first of string', equal("hola".first(), 'h')),
   // last
-  assert('last of list', [1, 2, 3].last(), 3),
-  assert('last of string', "hola".last(), 'a'),
+  assert('last of list',   equal([1, 2, 3].last(), 3)),
+  assert('last of string', equal("hola".last(), 'a')),
   // any
-  assert('any in case true - lists', [1,2,3].any(num => num > 2), true),
-  assert('any in case true - string', "hola".any(letter => letter === "o"), true),
-  assert('any in case false - lists', [1,2,3].any(num => num > 5), false),
-  assert('any in case false - lists', "hola".any(letter => letter === 'w'), false),
-  assert('any in case empty', [].any(_ => false), false),
+  assert('any in case true - lists',  isTrue([1,2,3].any(num => num > 2))),
+  assert('any in case true - string', isTrue("hola".any(letter => letter === "o"))),
+  assert('any in case false - lists', isFalse([1,2,3].any(num => num > 5))),
+  assert('any in case false - lists', isFalse("hola".any(letter => letter === "w"))),
+  assert('any in case empty',         isFalse([].any(_ => false))),
   // all
-  assert('all in case true - lists', [1,2,3].all(num => num < 4), true),
-  assert('all in case true - string', "hola".all(letter => letter.match(/[a-z]/i)), true),
-  assert('all in case false - lists', [1,2,3].all(num => num > 2), false),
-  assert('all in case false - lists', "aaah".all(letter => letter === "a"), false),
-  assert('all in case empty', [].all(_ => false), true),
+  assert('all in case true - lists',  isTrue([1,2,3].all(num => num < 4))),
+  assert('all in case true - string', isTrue("hola".all(letter => letter.match(/[a-z]/i)))),
+  assert('all in case false - lists', isFalse([1,2,3].all(num => num > 2))),
+  assert('all in case false - lists', isFalse("aaah".all(letter => letter === "a"))),
+  assert('all in case empty',         isTrue([].all(_ => false))),
+  // take
+  assert('take 2 on a list',   equal([3,2,1].take(2), [3,2])),
+  assert('take 2 on a string', equal("hola".take(2), "ho")),
+  // drop
+  assert('drop 2 on a list',   equal([3,2,1].drop(2), [1])),
+  assert('drop 2 on a string', equal("hola".drop(2), "la"))
 );
