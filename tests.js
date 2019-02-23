4e8928cea8191f5bc1d33b2a71ecd8120e3d9f62
Nahuel Garbezza <n.garbezza@gmail.com>
2019-02-23 12:02:27 -0300

    upgrade to latest version of testy :white_check_mark:

--- tests.js
+++ tests.js
@@ -1,59 +1,51 @@
-require('./oow');
-const { suite, test, assertTrue, assertFalse, assertEquals } = require('@ngarbezza/testy');
+'use strict';
 
-function assertEqual(actual, expected) {
-  let result;
-  if (expected instanceof Array) {
-    result = actual.equals(expected);
-  } else {
-    result = actual === expected;
-  }
-  return { success: result, failureMessage: `Expected ${expected} got ${actual}` };
-}
+require('./oow');
+const { suite, test, assert } = require('@pmoo/testy');
 
 suite('messages added to array and string', () => {
   // isEmpty
-  test('list is empty',   assertTrue([].isEmpty()));
-  test('string is empty', assertTrue("".isEmpty()));
+  test('list is empty',   () => assert.isTrue([].isEmpty()));
+  test('string is empty', () => assert.isTrue("".isEmpty()));
   // notEmpty
-  test('list is not empty',   assertTrue([1].notEmpty()));
-  test('string is not empty', assertTrue("a".notEmpty()));
+  test('list is not empty',  () => assert.isTrue([1].notEmpty()));
+  test('string is not empty', () => assert.isTrue("a".notEmpty()));
   // first
-  test('first of list',   assertEqual([1, 2, 3].first(), 1));
-  test('first of string', assertEqual("hola".first(), 'h'));
+  test('first of list',   () => assert.areEqual([1, 2, 3].first(), 1));
+  test('first of string', () => assert.areEqual("hola".first(), 'h'));
   // second
-  test('second of list',   assertEqual([1, 2, 3].second(), 2));
-  test('second of string', assertEqual("hola".second(), 'o'));
+  test('second of list',   () => assert.areEqual([1, 2, 3].second(), 2));
+  test('second of string', () => assert.areEqual('hola'.second(), 'o'));
   // third
-  test('third of list',   assertEqual([1, 2, 3].third(), 3));
-  test('third of string', assertEqual("hola".third(), 'l'));
+  test('third of list',   () => assert.areEqual([1, 2, 3].third(), 3));
+  test('third of string', () => assert.areEqual('hola'.third(), 'l'));
   // last
-  test('last of list',   assertEqual([1, 2, 3].last(), 3));
-  test('last of string', assertEqual("hola".last(), 'a'));
+  test('last of list',   () => assert.areEqual([1, 2, 3].last(), 3));
+  test('last of string', () => assert.areEqual('hola'.last(), 'a'));
   // any
-  test('any in case true - lists',  assertTrue([1,2,3].any(num => num > 2)));
-  test('any in case true - string', assertTrue("hola".any(letter => letter === "o")));
-  test('any in case false - lists', assertFalse([1,2,3].any(num => num > 5)));
-  test('any in case false - lists', assertFalse("hola".any(letter => letter === "w")));
-  test('any in case empty',         assertFalse([].any(_ => false)));
+  test('any in case true - lists',  () => assert.isTrue([1,2,3].any(num => num > 2)));
+  test('any in case true - string', () => assert.isTrue("hola".any(letter => letter === "o")));
+  test('any in case false - lists', () => assert.isFalse([1,2,3].any(num => num > 5)));
+  test('any in case false - lists', () => assert.isFalse("hola".any(letter => letter === "w")));
+  test('any in case empty',         () => assert.isFalse([].any(_ => false)));
   // all
-  test('all in case true - lists',  assertTrue([1,2,3].all(num => num < 4)));
-  test('all in case true - string', assertTrue("hola".all(letter => letter.match(/[a-z]/i))));
-  test('all in case false - lists', assertFalse([1,2,3].all(num => num > 2)));
-  test('all in case false - lists', assertFalse("aaah".all(letter => letter === "a")));
-  test('all in case empty',         assertTrue([].all(_ => false)));
+  test('all in case true - lists',  () => assert.isTrue([1,2,3].all(num => num < 4)));
+  test('all in case true - string', () => assert.isTrue('hola'.all(letter => letter.match(/[a-z]/i))));
+  test('all in case false - lists', () => assert.isFalse([1,2,3].all(num => num > 2)));
+  test('all in case false - lists', () => assert.isFalse('aaah'.all(letter => letter === 'a')));
+  test('all in case empty',         () => assert.isTrue([].all(_ => false)));
   // take
-  test('take 2 on a list',   assertEqual([3,2,1].take(2), [3,2]));
-  test('take 2 on a string', assertEqual("hola".take(2), "ho"));
+  test('take 2 on a list',   () => assert.areEqual([3,2,1].take(2), [3,2]));
+  test('take 2 on a string', () => assert.areEqual('hola'.take(2), 'ho'));
   // drop
-  test('drop 2 on a list',   assertEqual([3,2,1].drop(2), [1]));
-  test('drop 2 on a string', assertEqual("hola".drop(2), "la"));
+  test('drop 2 on a list',   () => assert.areEqual([3,2,1].drop(2), [1]));
+  test('drop 2 on a string', () => assert.areEqual('hola'.drop(2), 'la'));
   // compact
-  test('compact a list with nulls', assertEqual([1, null, 2].compact(), [1,2]));
-  test('compact a list with undefineds', assertEqual([undefined, 1, undefined, 2].compact(), [1,2]));
+  test('compact a list with nulls',      () => assert.areEqual([1, null, 2].compact(), [1,2]));
+  test('compact a list with undefineds', () => assert.areEqual([undefined, 1, undefined, 2].compact(), [1,2]));
   // count
-  test('counting on list', assertEqual([1, 42, 2, 23].count(n => n >= 2), 3));
-  test('counting on string', assertEqual("holaaaaa".count(l => l === "a"), 5));
+  test('counting on list',   () => assert.areEqual([1, 42, 2, 23].count(n => n >= 2), 3));
+  test('counting on string', () => assert.areEqual('holaaaaa'.count(l => l === "a"), 5));
   // filter on string
-  test('counting on string', assertEqual("asaoamaeatahaianaga".filter(l => l !== "a"), "something"));
-});
+  test('filtering on string', () => assert.areEqual('asaoamaeatahaianaga'.filter(l => l !== 'a'), 'something'));
+}).run();
